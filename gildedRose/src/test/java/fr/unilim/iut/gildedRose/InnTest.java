839ea070a0327f5d18cdb173980db6ccfbb0dbb1
macawotte <maxime.pichon87@outlook.fr>
2018-11-13 08:19:42 +0100

    commit initial : récupération du code legacy

---
+++ gildedRose/src/test/java/fr/unilim/iut/gildedRose/InnTest.java
@@ -0,0 +1,10 @@
+package fr.unilim.iut.gildedRose;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InnTest {
+	@Test
+	public void should_write_tests() {
+	}
+}
