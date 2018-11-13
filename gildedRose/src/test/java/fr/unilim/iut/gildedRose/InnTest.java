0591d8ff64f1423972e7c811fe7b4e6074ed1a76
macawotte <maxime.pichon87@outlook.fr>
2018-11-13 09:35:33 +0100

    Mise e place du harnai de tests sur le code legacy

--- gildedRose/src/test/java/fr/unilim/iut/gildedRose/InnTest.java
+++ gildedRose/src/test/java/fr/unilim/iut/gildedRose/InnTest.java
@@ -77,6 +77,31 @@ public class InnTest {
 	}
 	
 	
+	@Test
+	public void should_test_against_legacy_code() {
+		LegacyInn legacyInn = new LegacyInn();
+		LegacyInn inn = new LegacyInn();
+		
+		for (int day = 0; day < 1000; day++) {
+			List<Item> items = inn.getItems();
+			List<Item> legacyItems = legacyInn.getItems();
+			
+			assertThat(items).hasSize(legacyItems.size());
+
+			for (int i = 0; i < legacyItems.size(); i++) {
+				Item item = items.get(i);
+				Item legacyItem = legacyItems.get(i);
+				
+				assertThat(item.getName()).isEqualTo(legacyItem.getName());
+				assertThat(item.getQuality()).isEqualTo(legacyItem.getQuality());
+				assertThat(item.getSellIn()).isEqualTo(legacyItem.getSellIn());
+			}
+
+			inn.updateQuality();
+			legacyInn.updateQuality();
+		}
+	}
+	
 	
 	
 	
